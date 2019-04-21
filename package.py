de56e9d29516c7d05c50ad23040a00546733e1f9
Irving ST <irving@irving-st.id.au>
2019-04-22 01:31:20 +1000

    Switch to PyQt5 due to Pyinstaller having hiccups with PySide2. Packaging works.

--- package.py
+++ package.py
@@ -2,20 +2,34 @@
 
 # Packages the code into a standalone application
 # Is a python script since I dont want to write a separate shell script and bash script
+# https://stackoverflow.com/questions/37888581/pyinstaller-ui-files-filenotfounderror-errno-2-no-such-file-or-directory
 
 import os
 import subprocess
 
 filepath = os.path.dirname(os.path.abspath(__file__)) + '/ses/gui.py'
+name = 'SES-Rollout'
 
+# Prepare spec file
 cmd = []
-cmd.append('pyinstaller')
+cmd.append('pyi-makespec')
+cmd.append('--noconsole')
 cmd.append('--onefile')
-cmd.extend(['--name', 'SES-Rollout'])
-#cmd.append('--debug')
-# cmd.extend(['--paths', ''])
+#cmd.append('--onedir')
+cmd.extend(['--name', name])
+cmd.extend(['--add-data', 'qt/ses.ui'+os.pathsep+'qt/'])
 cmd.append(filepath)
 
 print(cmd)
+subprocess.run(cmd)
 
+# Run pyinstaller
+cmd = []
+cmd.append('pyinstaller')
+cmd.append(name+'.spec')
+
+#cmd.append('--debug')
+# cmd.extend(['--paths', ''])
+
+print(cmd)
 subprocess.run(cmd)
--- ses/gui.py
+++ ses/gui.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 
 # GUI interface to SES-Rollout
+# Helpful references: 
+# https://stackoverflow.com/questions/2398800/linking-a-qtdesigner-ui-file-to-python-pyqt
+# https://stackoverflow.com/questions/14892713/how-do-you-load-ui-files-onto-python-classes-with-pyside/14894550#14894550
+# https://stackoverflow.com/questions/37888581/pyinstaller-ui-files-filenotfounderror-errno-2-no-such-file-or-directory
 
 import os
 import sys
 
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import QFile
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QFile
+from PyQt5 import QtGui, uic
+
+
+# Define function to import external files when using PyInstaller.
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    return os.path.join(base_path, relative_path)
+
+class SESRMainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(SESRMainWindow, self).__init__()
+        filepath = resource_path('qt/ses.ui')
+        uic.loadUi(filepath, self)
+        self.show()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
-    filepath = os.path.dirname(os.path.abspath(__file__)) + '/../qt/ses.ui'
-    ui_file = QFile(filepath)
-    ui_file.open(QFile.ReadOnly)
-
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    window.show()
+    window = SESRMainWindow()
 
     sys.exit(app.exec_())
