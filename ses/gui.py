391730af35477e008f6dd5ced95235bd65d45639
Irving ST <irving@irving-st.id.au>
2019-04-22 02:43:14 +1000

    Starting connecting the frontend and backend

--- ses/gui.py
+++ ses/gui.py
@@ -13,6 +13,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import QFile
 from PyQt5 import QtGui, uic
 
+import announce
+import monitor_ses_selenium
 
 # Define function to import external files when using PyInstaller.
 def resource_path(relative_path):
@@ -27,10 +29,37 @@ def resource_path(relative_path):
 
 class SESRMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
+        # Initialisation stuff
         super(SESRMainWindow, self).__init__()
         filepath = resource_path('qt/ses.ui')
-        uic.loadUi(filepath, self)
-        self.show()
+        self.ui = uic.loadUi(filepath)
+        self.ui.show()
+
+        # Set up all our application behavior
+        self.ui.monitor_start.clicked.connect(self.monitor_start)
+
+
+    def monitor_start(self):
+        print('Starting_program!')
+
+        credentials = {}
+        credentials['login'] = self.ui.username.text() or ''
+        credentials['pass']  = self.ui.password.text() or ''
+
+        if len(credentials['login']) < 1 or len(credentials['pass']) < 1:
+            print("No login credentials set.")
+            return
+
+        headless = self.ui.browser_headless.isChecked()
+
+        if self.ui.rb_site_live.isChecked():
+            livesite = True
+        if self.ui.rb_site_training.isChecked():
+            livesite = False
+
+        announce.announceStartup(livesite)
+        # This works, but blocks the entire UI thread, so we need a separate worker thread
+        #monitor_ses_selenium.monitor_jobs(credentials, livesite, headless)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
